b03bc982df58cb81fe3ede523cb6b0c12122a7ab
Garrett Brown <garrettpaulbrown@gmail.com>
2020-12-31 13:02:02 -0800

    Initialize classes tests.

---
+++ BakeryTests/ModelTests/BreadTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bread.Models;
+using System;
+
+namespace Bread.TestTool
+{
+    [TestClass]
+    public class BreadTests
+    {
+        [TestMethod]
+         public void BreadPrice_Initialized_True()
+        {
+        //Test Method goes here 
+        }
+    }
+}
---
+++ BakeryTests/ModelTests/PastryTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Pastry.Models;
+using System;
+
+namespace Pastry.TestTool
+{
+    [TestClass]
+    public class PastryTests
+    {
+        [TestMethod]
+        public void PastryPrice_Initialized_True()
+        {
+        //Test Method goes here 
+        }
+    }
+}
